437d76b4e4654d85b08623a6ab369a961f3a7e2c
Andersonbaltazar <franklin.baltazar@tecsup.edu.pe>
2025-06-05 15:14:17 -0500

    ander

--- src/test/java/com/tecsup/petclinic/webs/VisitControllerTest.java
+++ src/test/java/com/tecsup/petclinic/webs/VisitControllerTest.java
@@ -1,11 +1,24 @@
 package com.tecsup.petclinic.webs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tecsup.petclinic.dtos.VisitDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -15,6 +28,30 @@ public class VisitControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+
+    @Test
+    public void testCreateVisit() throws Exception {
+
+        int PET_ID = 1;
+        LocalDate DATE = LocalDate.of(2024, 6, 5);
+        String DESCRIPTION = "Consulta de control";
+
+        VisitDTO newVisitDTO = new VisitDTO();
+        newVisitDTO.setPetId(PET_ID);
+        newVisitDTO.setDate(DATE);
+        newVisitDTO.setDescription(DESCRIPTION);
+
+        this.mockMvc.perform(post("/visits")
+                        .content(om.writeValueAsString(newVisitDTO))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+
+                .andExpect(jsonPath("$.petId", is(PET_ID)))
+                .andExpect(jsonPath("$.date", is(DATE.toString())))
+                .andExpect(jsonPath("$.description", is(DESCRIPTION)));
+    }
 
 
 }
